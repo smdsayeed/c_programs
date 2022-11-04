b89a782cd9e3c6b3cf09e6c34a68809072ebda8b
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-04 15:31:32 +0530

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+  int main (){
+    int ctr=1;
+    while (ctr<=10)
+    {
+        printf("\n %d \n",ctr);
+        ctr++;
+    }
+    printf("\n End of the Program");
+
+  return 0;
+}
