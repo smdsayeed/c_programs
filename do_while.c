bb7c64ee3c9d516a1906d7c21e42118fea3a5431
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-09 12:34:54 +0530

    Create do_while.c

---
+++ do_while.c
@@ -0,0 +1,17 @@
+
+
+//do while
+#include<stdio.h>
+#include<conio.h>
+  int main (){
+    int i=1,n;
+    printf("enter a number \n");
+    scanf("%d",&n);
+    do
+    {
+        printf("%i",i);i++;
+    } while (i<n);
+    
+printf("\n");
+  return 0;
+}
