52f7b4afdfbadc1810182fa52ebb792ef68fa131
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-14 15:49:09 +0530

    Create string_str.c

---
+++ string_str.c
@@ -0,0 +1,15 @@
+
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+  int main(){
+//strstr --find first occurences for the given string to the another string
+    char str[70]="Junior Software Developer in SKILL HUB-APSSDC";
+    printf("output string is :%s",strstr(str,"SKILL"));
+  }
+
+//output : SKILL HUB-APSSDC
