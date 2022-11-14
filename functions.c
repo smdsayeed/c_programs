3de58ad67cc72a51cc77685f196328375b31866a
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-14 12:20:46 +0530

    Update functions.c
    Taking inputs from the user

--- functions.c
+++ functions.c
@@ -6,7 +6,9 @@ int sum(int a , int b );
 
 
   int main(){
-  
+   int a,b;
+  printf("Enter two values for the summation\n");
+    scanf("%d %d",&a,&b);
  int r= sum(15,10);  //called function
     printf("The summation is :%d",r);
 
